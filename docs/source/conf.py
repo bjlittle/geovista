b2b2f5e2e0a08aac55d34c2d31536f8eec0db03a
Bill Little <bill.james.little@gmail.com>
2021-12-05 04:13:32 +0000

    blackify conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -102,9 +102,7 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_sidebars = {
-    "**": ["sidebar-ethical-ads.html"]
-}
+html_sidebars = {"**": ["sidebar-ethical-ads.html"]}
 
 
 # -- Pygments configuration --------------------------------------------------
