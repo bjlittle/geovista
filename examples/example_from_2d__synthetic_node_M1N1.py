4ce1925100ab34d282398c60412ba9279d0e0df4
Bill Little <bill.james.little@gmail.com>
2022-07-26 14:37:59 +0100

    panty examples for lfric orog + warp

--- examples/example_from_2d__synthetic_node_M1N1.py
+++ examples/example_from_2d__synthetic_node_M1N1.py
@@ -3,14 +3,17 @@ import numpy as np
 import geovista as gv
 import geovista.theme
 
+# create the 2D spatial coordinates and data
 M, N = 45, 90
 lats = np.linspace(-90, 90, M + 1)
 lons = np.linspace(-180, 180, N + 1)
 mlons, mlats = np.meshgrid(lons, lats, indexing="xy")
 data = np.random.random((M + 1) * (N + 1))
 
+# create the mesh from the synthetic data
 mesh = gv.Transform.from_2d(mlons, mlats, data=data, name="synthetic")
 
+# plot the mesh
 plotter = gv.GeoPlotter()
 plotter.add_mesh(mesh, cmap="tempo", show_edges=True)
 plotter.add_coastlines(resolution="10m", color="white")
--- examples/example_from_unstructured__lfric_orog.py
+++ examples/example_from_unstructured__lfric_orog.py
@@ -1,63 +1,28 @@
-import iris
-from iris.experimental.ugrid import PARSE_UGRID_ON_LOAD
-
 import geovista as gv
+from geovista.pantry import lfric_orog
 import geovista.theme
 
+# load the sample data
+sample = lfric_orog()
 
-def fix(cube):
-    import numpy as np
-
-    data = cube.data
-    wt = np.array([data[i] for i in range(10250, 10273 + 1)])
-    et = np.array([data[i] for i in range(10344, 10367 + 1)])
-    t = (wt + et) / 2
-
-    wm = np.array([data[i] for i in range(23, 2279 + 48, 48)])
-    em = np.array([data[i] for i in range(25, 2281 + 48, 48)])
-    m = (wm + em) / 2
-
-    wb = np.array([data[i] for i in range(12625, 12649 + 1)])
-    eb = np.array([data[i] for i in range(12529, 12553 + 1)])
-    b = (wb + eb) / 2
-
-    r = np.array([i for i in range(10297, 10320 + 1)])
-    data[r] = t
-
-    r = np.array([i for i in range(24, 2280 + 48, 48)])
-    data[r] = m
-
-    r = np.array([i for i in range(12577, 12601 + 1)])
-    data[r] = b
-
-    return data
-
-
-fname = "./qrparm_shared.orog.ugrid.node.nc"
-with PARSE_UGRID_ON_LOAD.context():
-    cube = iris.load_cube(fname, "surface_altitude")
-
-face_node = cube.mesh.face_node_connectivity
-indices = face_node.indices_by_location()
-lons, lats = cube.mesh.node_coords
-
-data = fix(cube)
-
+# create the mesh from the sample data
 mesh = gv.Transform.from_unstructured(
-    lons.points,
-    lats.points,
-    indices,
-    data=data,
-    start_index=face_node.start_index,
-    name=cube.name(),
+    sample.lons,
+    sample.lats,
+    sample.connectivity,
+    data=sample.data,
+    start_index=sample.start_index,
 )
 
+# plot the mesh
 plotter = gv.GeoPlotter()
-plotter.add_coastlines(resolution="50m", color="white")
-plotter.add_mesh(mesh, cmap="balance", show_edges=False)
+sargs = dict(title=f"{sample.name} / {sample.units}")
+plotter.add_mesh(mesh, cmap="balance", scalar_bar_args=sargs)
+resolution = "50m"
+plotter.add_coastlines(resolution=resolution, color="white")
 plotter.add_axes()
 plotter.add_text(
-    "Unstructured Cube-Sphere Node Data (N, 4)",
+    f"LFRic Unstructured Cube-Sphere ({resolution} Coastlines)",
     position="upper_left",
     font_size=10,
     shadow=True,
