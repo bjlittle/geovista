9af5214bd0660d21e1b3da7d3d8f6a478693a60a
Bill Little <bill.james.little@gmail.com>
2023-01-10 09:30:06 +0000

    comment update

--- src/geovista/examples/from_1d__oisst.py
+++ src/geovista/examples/from_1d__oisst.py
@@ -3,9 +3,9 @@
 This example demonstrates how to create a mesh from 1-D latitude and longitude
 (degrees) cell bounds.
 
-The data is NOAA/NECI 1/4° Daily Optimum Interpolation Sea Surface
-Temperature (OISST) v2.1 Advanced Very High Resolution Radiometer (AVHRR)
-gridded data (https://doi.org/10.25921/RE9P-PT57).
+It uses NOAA/NECI 1/4° Daily Optimum Interpolation Sea Surface Temperature
+(OISST) v2.1 Advanced Very High Resolution Radiometer (AVHRR) gridded data
+(https://doi.org/10.25921/RE9P-PT57).
 
 Note that, a threshold is also applied to remove land NaN cells, and a
 NASA Blue Marble base layer is rendered along with Natural Earth coastlines.
