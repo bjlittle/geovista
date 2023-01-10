0ade192bd42fefe78afa58f57aed0cc9bede5389
Bill Little <bill.james.little@gmail.com>
2023-01-10 14:12:48 +0000

    examples doc-string

---
+++ src/geovista/examples/__init__.py
@@ -0,0 +1,14 @@
+"""
+This module contains various geovista eye-candy examples which are
+importable and runnable as standalone scripts from the command line.
+
+Note that, the examples are installed as part of the geovista package
+and can be easily accessed through the geovista command line interface
+(CLI) entry-point.
+
+.. code: bash
+
+  # For further details refer to the geovista examples (CLI) help
+  geovista examples --help
+
+"""
