2e03541e89e5f7afe14365be3bd21050a388547c
Bill Little <bill.james.little@gmail.com>
2023-01-28 22:46:56 +0000

    add lam pacific projection example

---
+++ src/geovista/examples/from_unstructured__lam_pacific_moll.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+This example demonstrates how to create a mesh from CF UGRID 1-D latitude and
+longitude unstructured cell points and connectivity. The resulting mesh
+contains quad cells.
+
+It uses a high-resolution Local Area Model (LAM) mesh of air potential
+temperature data located on the mesh faces/cells.
+
+Note that, a Natural Earth texture is rendered as a base layer, and the
+mesh is transformed to the Mollweide pseudo-cylindrical projection.
+
+"""
+
+import geovista as gv
+from geovista.pantry import lam_pacific
+import geovista.theme  # noqa: F401
+
+
+def main() -> None:
+    # load the sample data
+    sample = lam_pacific()
+
+    # create the mesh from the sample data
+    mesh = gv.Transform.from_unstructured(
+        sample.lons,
+        sample.lats,
+        connectivity=sample.connectivity,
+        data=sample.data,
+    )
+
+    # provide mesh diagnostics via logging
+    gv.logger.info(f"{mesh}")
+
+    # plot the mesh
+    plotter = gv.GeoPlotter(crs=(projection := "+proj=moll"))
+    sargs = dict(title=f"{sample.name} / {sample.units}", shadow=True)
+    plotter.add_mesh(mesh, scalar_bar_args=sargs)
+    plotter.add_base_layer(texture=gv.natural_earth_hypsometric())
+    plotter.add_axes()
+    plotter.add_text(
+        f"CF UGRID LAM ({projection})",
+        position="upper_left",
+        font_size=10,
+        shadow=True,
+    )
+    plotter.view_xy()
+    plotter.camera.zoom(1.5)
+    plotter.show()
+
+
+if __name__ == "__main__":
+    main()
