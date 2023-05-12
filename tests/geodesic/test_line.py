71357fc0f87dac9795166d02e69b590a908623d8
Bill Little <bill.james.little@gmail.com>
2023-05-12 23:46:10 +0100

    add test coverage

---
+++ tests/geodesic/test_line.py
@@ -0,0 +1,57 @@
+"""Unit-tests for :func:`geovista.geodesic.line`."""
+import numpy as np
+import pytest
+
+from geovista.common import RADIUS, ZLEVEL_FACTOR, to_spherical
+from geovista.geodesic import GEODESIC_NPTS, line
+
+
+@pytest.mark.parametrize(
+    "lons,lats",
+    [(range(10), range(20)), (list(range(10)), list(range(20)))],
+)
+def test_lons_lats__size_unequal_fail(lons, lats):
+    emsg = "Require the same number"
+    with pytest.raises(ValueError, match=emsg):
+        _ = line(lons, lats)
+
+
+@pytest.mark.parametrize(
+    "lons,lats",
+    [(0, 1), ([0], [1])],
+)
+def test_lons_lats__size_minimal_fail(lons, lats):
+    emsg = "containing at least 2 longitude/latitude"
+    with pytest.raises(ValueError, match=emsg):
+        _ = line(lons, lats)
+
+
+def test_lons_lats__loop_minimal_fail():
+    lons = lats = [0, 0]
+    emsg = "containing at least 3 longitude/latitude"
+    with pytest.raises(ValueError, match=emsg):
+        _ = line(lons, lats)
+
+
+@pytest.mark.parametrize(
+    "nsamples,npts", [(2, None), (2, 64), (3, 128), (4, 256), (8, 512)]
+)
+def test_npts(nsamples, npts):
+    lons = lats = range(nsamples)
+    result = line(lons, lats, npts=npts)
+    if npts is None:
+        npts = GEODESIC_NPTS
+    n_points = (nsamples - 1) * npts + 1
+    assert result.n_points == n_points
+
+
+@pytest.mark.parametrize(
+    "nsamples",
+    range(2, 11),
+)
+def test_contains_sample_points(nsamples):
+    lons = lats = np.arange(nsamples, dtype=float)
+    result = line(lons, lats)
+    radius = RADIUS + RADIUS * ZLEVEL_FACTOR
+    xyz = to_spherical(lons, lats, radius=radius)
+    np.testing.assert_array_equal(xyz, result.points[::GEODESIC_NPTS])
