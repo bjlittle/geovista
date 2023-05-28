0f4da14788a65be10bc4d5bd13c85ad29b72d5f9
Bill Little <bill.james.little@gmail.com>
2023-05-28 23:27:34 +0100

    add crs to coastlines mesh on load

--- tests/geometry/conftest.py
+++ tests/geometry/conftest.py
@@ -2,8 +2,16 @@
 
 import pytest
 
+from geovista.crs import WGS84
+
 
 @pytest.fixture(params=["110m", "50m", "10m"])
 def resolution(request):
     """Fixture for testing each of the Natural Earth coastline resolutions."""
     return request.param
+
+
+@pytest.fixture
+def wgs84_wkt():
+    """Fixture for generating WG284 CRS WKT as a string."""
+    return WGS84.to_wkt()
