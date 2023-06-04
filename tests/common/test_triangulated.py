1ef300d67fc89c00cf7283cad405dc4f380503ee
Bill Little <bill.james.little@gmail.com>
2023-06-04 23:56:27 +0100

    extend common test coverage (#304)

---
+++ tests/common/test_triangulated.py
@@ -0,0 +1,13 @@
+"""Unit-tests for :func:`geovista.common.triangulated`."""
+from geovista.common import triangulated
+
+
+def test_not_triangulated(lam_uk):
+    """Test detection of untriangulated mesh."""
+    assert not triangulated(lam_uk)
+
+
+def test_triangulated(lam_uk):
+    """Test detection of triangulated mesh."""
+    mesh = lam_uk.triangulate()
+    assert triangulated(mesh)
