333ae37f363d5f269a599245e7f31e4a025a2f65
Bill Little <bill.james.little@gmail.com>
2023-06-13 23:11:02 +0100

    add dynamico icosahedral pantry, sample and example

--- src/geovista/examples/from_unstructured__icosahedral.py
+++ src/geovista/examples/from_unstructured__icosahedral.py
@@ -3,12 +3,12 @@
 
 Notes
 -----
-.. versionadded:: 0.1.0
+.. versionadded:: 0.3.0
 
 """
 
 import geovista as gv
-from geovista.pantry import hexahedron
+from geovista.pantry import icosahedral
 import geovista.theme  # noqa: F401
 
 
@@ -17,13 +17,16 @@ def main() -> None:
 
     The resulting mesh contains 6-sided (hexagonal) cells.
 
-    The data is synthetically generated and targets the mesh faces/cells.
+    It uses surface air pressure data from the DYNAMICO project, a new dynamical core
+    for the Laboratoire de Météorologie Dynamique (LMD-Z), the atmospheric General
+    Circulation Model (GCM) part of Institut Pierre-Simon Laplace (IPSL-CM) Earth
+    System Model. The data targets the mesh faces/cells.
 
-    Note that, the mesh is transformed to the Polyconic pseudo-conical projection.
+    Note that, Natural Earth coastlines are also rendered.
 
     """
     # load the sample data
-    sample = hexahedron()
+    sample = icosahedral()
 
     # create the mesh from the sample data
     mesh = gv.Transform.from_unstructured(sample.lons, sample.lats, data=sample.data)
@@ -32,18 +35,18 @@ def main() -> None:
     gv.logger.info("%s", mesh)
 
     # plot the mesh
-    plotter = gv.GeoPlotter(crs=(projection := "+proj=poly"))
+    plotter = gv.GeoPlotter()
     sargs = {"title": f"{sample.name} / {sample.units}", "shadow": True}
     plotter.add_mesh(mesh, scalar_bar_args=sargs)
+    plotter.add_coastlines()
     plotter.add_axes()
     plotter.add_text(
-        f"DYNAMICO Hexahedron ({projection})",
+        "DYNAMICO Icosahedral (10m Coastlines)",
         position="upper_left",
         font_size=10,
         shadow=True,
     )
-    plotter.view_xy()
-    plotter.camera.zoom(1.5)
+    plotter.view_xz(negative=True)
     plotter.show()
 
 
--- src/geovista/pantry.py
+++ src/geovista/pantry.py
@@ -24,8 +24,8 @@ from .common import LRU_CACHE_SIZE
 __all__ = [
     "fesom",
     "fvcom_tamar",
-    "hexahedron",
     "icon_soil",
+    "icosahedral",
     "lam_equator",
     "lam_falklands",
     "lam_london",
@@ -228,43 +228,6 @@ def fvcom_tamar() -> SampleUnstructuredXY:
 
 
 @lru_cache(maxsize=LRU_CACHE_SIZE)
-def hexahedron() -> SampleUnstructuredXY:
-    """Download and cache unstructured surface sample data.
-
-    Load DYNAMICO hexahedron unstructured mesh.
-
-    Returns
-    -------
-    SampleUnstructuredXY
-        The hexagonal unstructured spatial coordinates and data payload.
-
-    Notes
-    -----
-    .. versionadded:: 0.1.0
-
-    """
-    fname = "hexahedron.nc"
-    processor = pooch.Decompress(method="auto", name=fname)
-    resource = CACHE.fetch(f"pantry/{fname}.bz2", processor=processor)
-    dataset = nc.Dataset(resource)
-
-    # load the lon/lat hex cell grid
-    lons = dataset.variables["bounds_lon_i"][:]
-    lats = dataset.variables["bounds_lat_i"][:]
-
-    # load the mesh payload
-    data = dataset.variables["phis"][:]
-    name = capitalise("synthetic")
-    units = "1"
-
-    sample = SampleUnstructuredXY(
-        lons, lats, lons.shape, data=data, name=name, units=units
-    )
-
-    return sample
-
-
-@lru_cache(maxsize=LRU_CACHE_SIZE)
 def icon_soil() -> SampleUnstructuredXY:
     """Download and cache unstructured surface sample data.
 
@@ -300,6 +263,43 @@ def icon_soil() -> SampleUnstructuredXY:
     data = dataset.variables["SOILTYP"][:]
     name = capitalise("soil type")
     units = "1"
+
+    sample = SampleUnstructuredXY(
+        lons, lats, lons.shape, data=data, name=name, units=units
+    )
+
+    return sample
+
+
+@lru_cache(maxsize=LRU_CACHE_SIZE)
+def icosahedral() -> SampleUnstructuredXY:
+    """Download and cache unstructured surface sample data.
+
+    Load DYNAMICO icosahedral unstructured mesh.
+
+    Returns
+    -------
+    SampleUnstructuredXY
+        The hexagonal unstructured spatial coordinates and data payload.
+
+    Notes
+    -----
+    .. versionadded:: 0.3.0
+
+    """
+    fname = "dynamico_icosahedral.nc"
+    processor = pooch.Decompress(method="auto", name=fname)
+    resource = CACHE.fetch(f"pantry/{fname}.bz2", processor=processor)
+    dataset = nc.Dataset(resource)
+
+    # load the lon/lat hex cell grid
+    lons = dataset.variables["lon_bnds"][:]
+    lats = dataset.variables["lat_bnds"][:]
+
+    # load the mesh payload
+    data = dataset.variables["ps"]
+    name = capitalise(data.long_name)
+    units = data.units
 
     sample = SampleUnstructuredXY(
         lons, lats, lons.shape, data=data, name=name, units=units
