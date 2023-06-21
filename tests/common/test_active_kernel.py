2d63dd94f3a0760e8931eb42cc22c592aace4882
Bill Little <bill.james.little@gmail.com>
2023-06-21 16:58:33 +0100

    geovista.common.activte_kernel test coverage

---
+++ tests/common/test_active_kernel.py
@@ -0,0 +1,25 @@
+"""Unit-tests for :func:`geovista.common.active_kernel`."""
+import pytest
+
+from geovista.common import active_kernel
+
+
+def test_kernel_none():
+    """Test with no ipython kernel."""
+    assert not active_kernel()
+
+
+@pytest.mark.parametrize("exception", [AttributeError, ModuleNotFoundError])
+def test_kernel__exception(mocker, exception):
+    """Test with a mock ipython kernel raising exceptions."""
+    module = mocker.MagicMock(get_ipython=mocker.MagicMock(side_effect=exception))
+    mocker.patch.dict("sys.modules", IPython=module)
+    assert not active_kernel()
+
+
+def test_kernel_active(mocker):
+    """Test with a mock active ipython kernel."""
+    kernel = mocker.sentinel.kernel
+    module = mocker.MagicMock(get_ipython=mocker.MagicMock(kernel=kernel))
+    mocker.patch.dict("sys.modules", IPython=module)
+    assert active_kernel()
