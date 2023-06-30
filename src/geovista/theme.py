41e190a513558157da7ec499368d567cd919a2ae
Bill Little <bill.james.little@gmail.com>
2023-06-30 23:42:28 +0100

    update geovista.theme and min pin pyvista>=0.40

--- src/geovista/theme.py
+++ src/geovista/theme.py
@@ -7,11 +7,12 @@ Notes
 """
 import pyvista as pv
 
-theme = pv.themes.DefaultTheme()
+theme = pv.themes.Theme()
 theme.name = "geovista"
 theme.background = (1.0, 1.0, 1.0)
+theme.color = "lightgray"
 theme.cmap = "balance"
-theme.edge_color = "grey"
+theme.edge_color = "gray"
 theme.font.color = (0.0, 0.0, 0.0)
 theme.outline_color = (0.0, 0.0, 0.0)
 theme.title = "GeoVista"
