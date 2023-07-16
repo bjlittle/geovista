bbfad38df4f8bab0bf2300ae23fc6bc42635be8e
Tetsuo Koyama <tkoyama010@gmail.com>
2023-07-16 15:57:37 -0500

    Add an easy wind vector example (#387)
    * Copy arrows from cartopy
    
    * Rename
    
    * Add vectors.py
    
    * Update vectors.py
    
    * Update vectors.py
    
    * Update vectors.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Apply suggestions from code review
    
    Co-authored-by: Bill Little <bill.little@metoffice.gov.uk>
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Bill Little <bill.little@metoffice.gov.uk>

---
+++ src/geovista/examples/vectors.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Importable and runnable geovista example.
+
+Notes
+-----
+.. versionadded:: 0.4.0
+
+"""
+from __future__ import annotations
+
+import numpy as np
+import pyvista as pv
+
+import geovista
+import geovista as gv
+import geovista.theme
+
+
+def main() -> None:
+    """Create vectors plotting inspired by cartopy."""
+    # make cool swirly pattern
+    sphere = pv.Sphere(gv.common.RADIUS)
+    vectors = np.vstack(
+        (
+            -sphere.points[:, 1],
+            sphere.points[:, 0],
+            sphere.points[:, 2] * 0.0,
+        )
+    ).T
+    sphere["vectors"] = vectors * 0.1
+    sphere.set_active_vectors("vectors")
+
+    # plot mesh
+    plotter = gv.GeoPlotter()
+    plotter.enable_anti_aliasing("ssaa")
+    plotter.add_base_layer(lighting=False)
+    plotter.add_coastlines(color="black", lighting=False)
+    plotter.add_mesh(sphere.arrows, lighting=False)
+    plotter.camera.zoom(1.5)
+    plotter.add_axes()
+    plotter.show()
+
+
+if __name__ == "__main__":
+    main()
