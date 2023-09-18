67567e80523f036347bf87b8d6818fabf13dbd1e
Bill Little <bill.james.little@gmail.com>
2023-09-18 13:49:01 +0100

    fix mocker patch pattern

--- tests/geometry/test_coastlines.py
+++ tests/geometry/test_coastlines.py
@@ -8,11 +8,13 @@ from geovista.geometry import coastlines
 def test_defaults(mocker):
     """Test expected defaults are honoured."""
     mesh = mocker.sentinel.mesh
-    fetch = mocker.patch("geovista.geometry.fetch_coastlines", return_value=mesh)
-    resize = mocker.patch("geovista.geometry.resize")
+    _ = mocker.patch("geovista.geometry.fetch_coastlines", return_value=mesh)
+    _ = mocker.patch("geovista.geometry.resize")
     result = coastlines()
     assert result == mesh
-    fetch.assert_called_once_with(resolution=COASTLINES_RESOLUTION)
+    from geovista.geometry import fetch_coastlines, resize
+
+    fetch_coastlines.assert_called_once_with(resolution=COASTLINES_RESOLUTION)
     resize.assert_called_once_with(
         mesh, radius=None, zlevel=1, zscale=None, inplace=True
     )
@@ -25,23 +27,27 @@ def test_resize_kwarg_pass_thru(mocker):
     radius = mocker.sentinel.radius
     zscale = mocker.sentinel.zscale
     zlevel = mocker.sentinel.zlevel
-    fetch = mocker.patch("geovista.geometry.fetch_coastlines", return_value=mesh)
-    resize = mocker.patch("geovista.geometry.resize")
+    _ = mocker.patch("geovista.geometry.fetch_coastlines", return_value=mesh)
+    _ = mocker.patch("geovista.geometry.resize")
     kwargs = {"radius": radius, "zscale": zscale, "zlevel": zlevel}
     result = coastlines(resolution=resolution, **kwargs)
     assert result == mesh
-    fetch.assert_called_once_with(resolution=resolution)
+    from geovista.geometry import fetch_coastlines, resize
+
+    fetch_coastlines.assert_called_once_with(resolution=resolution)
     resize.assert_called_once_with(mesh, **kwargs, inplace=True)
 
 
 def test_fetch_exception(mocker):
     """Test coastlines are loaded on cache fetch failure."""
     mesh = mocker.sentinel.mesh
-    fetch = mocker.patch("geovista.geometry.fetch_coastlines", side_effect=ValueError)
-    load = mocker.patch("geovista.geometry.load_coastlines", return_value=mesh)
-    resize = mocker.patch("geovista.geometry.resize")
+    _ = mocker.patch("geovista.geometry.fetch_coastlines", side_effect=ValueError)
+    _ = mocker.patch("geovista.geometry.load_coastlines", return_value=mesh)
+    _ = mocker.patch("geovista.geometry.resize")
     result = coastlines()
     assert result == mesh
-    assert fetch.call_count == 1
+    from geovista.geometry import fetch_coastlines, load_coastlines, resize
+
+    assert fetch_coastlines.call_count == 1
     assert resize.call_count == 1
-    load.assert_called_once_with(resolution=COASTLINES_RESOLUTION)
+    load_coastlines.assert_called_once_with(resolution=COASTLINES_RESOLUTION)
