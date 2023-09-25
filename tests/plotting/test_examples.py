328bdf4cfa805d57618e3d7a6e7300d9e8c8376d
Tetsuo Koyama <tkoyama010@gmail.com>
2023-09-26 07:03:03 +0900

    Apply suggestions from code review
    Co-authored-by: Bill Little <bill.james.little@gmail.com>

--- tests/plotting/test_examples.py
+++ tests/plotting/test_examples.py
@@ -1,33 +1,8 @@
-"""Unit-tests for :class:`geovista.examples`."""
+"""Unit-tests for :mod:`geovista.examples`."""
 
-import geovista as gv
-from geovista.pantry import usgs_earthquakes
+from geovista.examples import earthquakes
 
 
 def test_earthquakes(verify_image_cache):
-    """Test earthquakes."""
-    sample = usgs_earthquakes()
-    plotter = gv.GeoPlotter(off_screen=True)
-    sargs = {"title": "Magnitude", "shadow": True}
-    plotter.add_points(
-        xs=sample.lons,
-        ys=sample.lats,
-        cmap="fire_r",
-        render_points_as_spheres=True,
-        scalars=sample.data,
-        point_size=5,
-        scalar_bar_args=sargs,
-    )
-    plotter.add_base_layer(texture=gv.natural_earth_1(), zlevel=0)
-    plotter.add_graticule()
-    plotter.add_coastlines()
-    plotter.add_axes()
-    plotter.add_text(
-        "USGS M2.5+ Earthquakes, 2000-2018",
-        position="upper_left",
-        font_size=10,
-        shadow=True,
-    )
-    plotter.view_xz(negative=True)
-    plotter.camera.zoom(1.5)
-    plotter.show()
+    """Test earthquakes example."""
+    earthquakes.main(off_screen=True)
