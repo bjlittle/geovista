c9c6b3308f9de514e4a944991ed38912a62e0a33
Tetsuo Koyama <tkoyama010@gmail.com>
2023-09-26 07:47:16 +0900

    Update test_examples.py

--- tests/plotting/test_examples.py
+++ tests/plotting/test_examples.py
@@ -5,5 +5,4 @@ from geovista.examples import earthquakes
 
 def test_earthquakes(verify_image_cache):
     """Test earthquakes example."""
-    plotter = earthquakes.main(off_screen=True)
-    plotter.show()
+    earthquakes.main(off_screen=True)
