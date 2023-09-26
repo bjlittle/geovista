69c10b86c09c28f4a85a2af29d28ac7c8ee5ee50
Tetsuo Koyama <tkoyama010@gmail.com>
2023-09-26 13:11:51 +0900

    Revert to 7220cbf66cb650134c3727e4cc465b2c138bf52b

--- tests/plotting/test_examples.py
+++ tests/plotting/test_examples.py
@@ -1,34 +1,13 @@
-"""Unit-tests for :mod:`geovista.examples`."""
-from __future__ import annotations
-
-from warnings import warn
-
-import pyvista as pv
+"""Unit-tests for :class:`geovista.examples`."""
 
 import geovista as gv
 from geovista.pantry import usgs_earthquakes
-import geovista.theme  # noqa: F401
-
-pv.OFF_SCREEN = True
 
 
 def test_earthquakes(verify_image_cache):
-    """Test earthquakes example."""
-    # load sample data, which requires the optional package
-    # dependencies 'pandas' and 'fastparquet'
-    try:
-        sample = usgs_earthquakes()
-    except ImportError:
-        wmsg = (
-            "Missing optional dependencies 'pandas' and 'fastparquet' are "
-            "required for the 'earthquakes' example. Use pip or conda to "
-            "install them."
-        )
-        warn(wmsg, stacklevel=2)
-        return
-
-    # plot the mesh
-    plotter = gv.GeoPlotter()
+    """Test earthquakes."""
+    sample = usgs_earthquakes()
+    plotter = gv.GeoPlotter(off_screen=True)
     sargs = {"title": "Magnitude", "shadow": True}
     plotter.add_points(
         xs=sample.lons,
@@ -39,7 +18,6 @@ def test_earthquakes(verify_image_cache):
         point_size=5,
         scalar_bar_args=sargs,
     )
-    # force zlevel alignment of coastlines and base layer
     plotter.add_base_layer(texture=gv.natural_earth_1(), zlevel=0)
     plotter.add_graticule()
     plotter.add_coastlines()
