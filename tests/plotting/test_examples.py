91debc83db6cb39842f22ad917fdf21f6a5d05e2
Bill Little <bill.james.little@gmail.com>
2023-09-27 22:38:09 +0100

    try pytest-xvfb

--- tests/plotting/test_examples.py
+++ tests/plotting/test_examples.py
@@ -5,4 +5,6 @@ from geovista.examples import earthquakes
 
 def test_earthquakes(verify_image_cache):
     """Image test of earthquakes examples."""
+    verify_image_cache.high_variance_test = True
+    verify_image_cache.var_error_value = 4470
     earthquakes.main(off_screen=True)
