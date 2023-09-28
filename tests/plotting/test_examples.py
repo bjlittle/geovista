ee7db4162610806aebb05765c90eb757301ea373
Bill Little <bill.james.little@gmail.com>
2023-09-28 16:56:27 +0100

    Elevate high variance warning level for image tests (#476)

--- tests/plotting/test_examples.py
+++ tests/plotting/test_examples.py
@@ -10,5 +10,6 @@ pv.OFF_SCREEN = True
 def test_earthquakes(verify_image_cache):
     """Image test of earthquakes example."""
     verify_image_cache.high_variance_test = True
-    verify_image_cache.var_error_value = 4470
+    verify_image_cache.var_error_value = (value := 4470)
+    verify_image_cache.var_warning_value = value
     earthquakes.main()
