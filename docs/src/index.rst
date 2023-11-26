2f5ca830275555f8146a136ed6a105bd5b83eba2
Bill Little <bill.james.little@gmail.com>
2023-11-26 23:07:16 +0000

    add sphinx-gallery support

--- docs/src/index.rst
+++ docs/src/index.rst
@@ -1,23 +1,5 @@
 .. title:: GeoVista
 
-.. jupyter-execute::
-   :hide-code:
-
-   # configure pyvista for pythreejs
-   import pyvista
-
-   pyvista.set_jupyter_backend("pythreejs")
-   pyvista.global_theme.background = "white"
-   pyvista.global_theme.window_size = [700, 500]
-   pyvista.global_theme.axes.show = True
-   # pyvista.global_theme.anti_aliasing = "fxaa"
-
-
-.. toctree::
-   :maxdepth: 1
-   :caption: Contents:
-
-
 .. raw:: html
 
    <h1 align="center">
@@ -30,29 +12,8 @@
 
 We're just bootstrapping ``geovista`` and its documentation, so please be patient 👍
 
-In the meantime, here's an interactive ``geovista`` amuse-bouche to whet your appetite...
+.. toctree::
+   :maxdepth: 1
+   :hidden:
 
-.. jupyter-execute::
-
-   import geovista as gv
-   from geovista.pantry import lfric_sst
-
-
-   # Load the Met Office LFRic C48 unstructured cube-sphere sample data.
-   sample = lfric_sst()
-
-   # Create the mesh from the sample data.
-   mesh = gv.Transform.from_unstructured(
-       sample.lons,
-       sample.lats,
-       connectivity=sample.connectivity,
-       data=sample.data,
-   )
-
-   # Plot the mesh.
-   plotter = gv.GeoPlotter()
-   sargs = dict(title=f"{sample.name} / {sample.units}")
-   plotter.add_mesh(mesh, scalar_bar_args=sargs, cmap="balance", show_edges=True)
-   plotter.add_coastlines()
-   plotter.add_axes()
-   plotter.show()
+   generated/gallery/index
--- src/geovista/examples/point_cloud/from_points__orca_cloud_eqc.py
+++ src/geovista/examples/point_cloud/from_points__orca_cloud_eqc.py
@@ -4,13 +4,26 @@
 # This file is part of GeoVista and is distributed under the 3-Clause BSD license.
 # See the LICENSE file in the package root directory for licensing details.
 
-"""Importable and runnable geovista example.
-
-Notes
------
-.. versionadded:: 0.2.0
-
 """
+ORCA2 Point Cloud Projection
+----------------------------
+
+This example demonstrates how to render a projected point cloud.
+
+Create a point-cloud from 1-D latitude, longitude and z-levels.
+
+The resulting mesh contains only points.
+
+Based on a curvilinear ORCA2 global ocean with tri-polar model grid of
+sea water potential temperature data, which has been reduced to a limited
+area and pre-filtered for temperature gradients.
+
+Note that, Natural Earth coastlines are also rendered along with a Natural
+Earth base layer with opacity. Additionally, the mesh is transformed to
+the Equidistant Cylindrical (Plate Carrée) conformal cylindrical
+projection.
+
+"""  # noqa: D205,D400
 from __future__ import annotations
 
 import geovista as gv
@@ -20,22 +33,18 @@ import geovista.theme
 
 
 def main() -> None:
-    """Create a point-cloud from 1-D latitude, longitude and z-levels.
+    """
+    Plot a projected point cloud.
 
-    The resulting mesh contains only points.
-
-    Based on a curvilinear ORCA2 global ocean with tri-polar model grid of
-    sea water potential temperature data, which has been reduced to a limited
-    area and pre-filtered for temperature gradients.
-
-    Note that, Natural Earth coastlines are also rendered along with a Natural
-    Earth base layer with opacity.
+    Notes
+    -----
+    .. versionadded:: 0.3.0
 
     """
-    # load the sample data
+    # Load the sample data.
     sample = um_orca2_gradient()
 
-    # create the point-cloud from the sample data
+    # Create the point cloud from the sample data.
     cloud = gv.Transform.from_points(
         sample.lons,
         sample.lats,
@@ -44,12 +53,14 @@ def main() -> None:
         zlevel=-sample.zlevel,
         zscale=ZLEVEL_SCALE_CLOUD,
     )
-
-    # provide cloud diagnostics via logging
+    # sphinx_gallery_start_ignore
+    # Provide mesh diagnostics via logging.
     gv.logger.info("%s", cloud)
+    # sphinx_gallery_end_ignore
 
-    # plot the point-cloud
-    plotter = gv.GeoPlotter()
+    # Plot the projected point cloud.
+    crs = "+proj=eqc"
+    plotter = gv.GeoPlotter(crs=crs)
     sargs = {"title": f"{sample.name} / {sample.units}", "shadow": True}
     plotter.add_mesh(
         cloud,
@@ -59,12 +70,12 @@ def main() -> None:
         render_points_as_spheres=True,
     )
     plotter.add_coastlines(color="black")
-    # force zlevel alignment of coastlines and base layer
+    # Force zlevel alignment of coastlines and base layer.
     plotter.add_base_layer(texture=gv.natural_earth_1(), opacity=0.5, zlevel=0)
     plotter.add_axes()
-    plotter.view_yz()
+    plotter.view_xy()
     plotter.add_text(
-        "ORCA Point-Cloud (10m Coastlines)",
+        f"ORCA Point-Cloud ({crs})",
         position="upper_left",
         font_size=10,
         shadow=True,
