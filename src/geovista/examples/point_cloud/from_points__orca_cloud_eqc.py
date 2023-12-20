4964536b0c595c4fbaa843e2b4549d5101b38b13
Bill Little <bill.james.little@gmail.com>
2023-12-20 23:43:28 +0000

    point cloud example retitle (#583)

--- src/geovista/examples/point_cloud/from_points__orca_cloud_eqc.py
+++ src/geovista/examples/point_cloud/from_points__orca_cloud_eqc.py
@@ -5,8 +5,8 @@
 # See the LICENSE file in the package root directory for licensing details.
 
 """
-ORCA2 Point Cloud Projection
-----------------------------
+ORCA2 Point Cloud (Projected)
+-----------------------------
 
 This example demonstrates how to render a projected point cloud.
 
