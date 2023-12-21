35894aaf77ee95544a474e80036e0e361bd77112
Bill Little <bill.james.little@gmail.com>
2023-12-21 01:38:22 +0000

    create sphinx-gallery warp section

--- src/geovista/examples/rectilinear/README.rst
+++ src/geovista/examples/rectilinear/README.rst
@@ -1,2 +1,2 @@
-Rectilinear Grids
-=================
+Rectilinear Grid
+================
--- src/geovista/examples/warp/from_unstructured__fvcom.py
+++ src/geovista/examples/warp/from_unstructured__fvcom.py
@@ -4,13 +4,28 @@
 # This file is part of GeoVista and is distributed under the 3-Clause BSD license.
 # See the LICENSE file in the package root directory for licensing details.
 
-"""Importable and runnable geovista example.
-
-Notes
------
-.. versionadded:: 0.1.0
-
 """
+FVCOM Bathymetry
+----------------
+
+This example demonstrates how to render a warped unstructured triangular mesh.
+
+📋 Summary
+^^^^^^^^^^
+
+Creates a mesh from 1-D latitude and longitude unstructured cell points.
+
+The resulting mesh contains triangular cells. The connectivity is required to
+construct the cells from the unstructured points.
+
+It uses an unstructured grid Finite Volume Community Ocean Model (FVCOM) mesh of
+sea floor depth below geoid data.
+
+Note that, the data is on the mesh faces/cells, but also on the nodes/points
+which can then be used to extrude the mesh to reveal the bathymetry of the
+Plymouth Sound and Tamar River in Cornwall, UK.
+
+"""  # noqa: D205,D212,D400
 from __future__ import annotations
 
 import geovista as gv
@@ -19,23 +34,17 @@ import geovista.theme
 
 
 def main() -> None:
-    """Create a mesh from 1-D latitude and longitude unstructured cell points.
+    """Plot a warped FVCOM unstructured mesh.
 
-    The resulting mesh contains triangular cells. The connectivity is required to
-    construct the cells from the unstructured points.
-
-    It uses an unstructured grid Finite Volume Community Ocean Model (FVCOM) mesh of
-    sea floor depth below geoid data.
-
-    Note that, the data is on the mesh faces/cells, but also on the nodes/points
-    which can then be used to extrude the mesh to reveal the bathymetry of the
-    Plymouth Sound and Tamar River in Cornwall, UK.
+    Notes
+    -----
+    .. versionadded:: 0.1.0
 
     """
-    # load the sample data
+    # Load the sample data.
     sample = fvcom_tamar()
 
-    # create the mesh from the sample data
+    # Create the mesh from the sample data.
     mesh = gv.Transform.from_unstructured(
         sample.lons,
         sample.lats,
@@ -43,16 +52,17 @@ def main() -> None:
         data=sample.face,
         name="face",
     )
-
-    # provide mesh diagnostics via logging
+    # sphinx_gallery_start_ignore
+    # Provide mesh diagnostics via logging.
     gv.logger.info("%s", mesh)
+    # sphinx_gallery_end_ignore
 
-    # warp the mesh nodes by the bathymetry
+    # Warp the mesh nodes by the bathymetry.
     mesh.point_data["node"] = sample.node
     mesh.compute_normals(cell_normals=False, point_normals=True, inplace=True)
     mesh.warp_by_scalar(scalars="node", inplace=True, factor=2e-5)
 
-    # plot the mesh
+    # Plot the unstructured mesh.
     plotter = gv.GeoPlotter()
     sargs = {"title": f"{sample.name} / {sample.units}", "shadow": True}
     plotter.add_mesh(mesh, cmap="deep", scalars="face", scalar_bar_args=sargs)
