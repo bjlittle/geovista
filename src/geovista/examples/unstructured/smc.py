d1c3e203d03051e9cdbcc7d6f20862147665936f
Bill Little <bill.james.little@gmail.com>
2023-12-23 00:18:18 +0000

    smc sphinx-gallery examples

--- src/geovista/examples/unstructured/smc.py
+++ src/geovista/examples/unstructured/smc.py
@@ -4,13 +4,26 @@
 # This file is part of GeoVista and is distributed under the 3-Clause BSD license.
 # See the LICENSE file in the package root directory for licensing details.
 
-"""Importable and runnable geovista example.
-
-Notes
------
-.. versionadded:: 0.1.0
-
 """
+Spherical Multi-Cell Mesh
+-------------------------
+
+This example demonstrates how to render an unstructured quadrilateral mesh.
+
+📋 Summary
+^^^^^^^^^^
+
+Creates a mesh from 2-D latitude and longitude unstructured cell bounds.
+
+The resulting mesh contains quad cells.
+
+It uses WAVEWATCH III (WW3) unstructured Spherical Multi-Cell (SMC) sea surface
+wave significant height data located on mesh faces/cells.
+
+Note that, a threshold is also applied to remove land ``NaN`` cells, and a
+Natural Earth base layer is rendered along with Natural Earth coastlines.
+
+"""  # noqa: D205,D212,D400
 from __future__ import annotations
 
 import geovista as gv
@@ -19,30 +32,27 @@ import geovista.theme
 
 
 def main() -> None:
-    """Create a mesh from 2-D latitude and longitude unstructured cell bounds.
+    """Plot an SMC unstructured mesh.
 
-    The resulting mesh contains quad cells.
-
-    It uses WAVEWATCH III (WW3) unstructured Spherical Multi-Cell (SMC) sea surface
-    wave significant height data located on mesh faces/cells.
-
-    Note that, a threshold is also applied to remove land ``NaN`` cells, and a
-    Natural Earth base layer is rendered along with Natural Earth coastlines.
+    Notes
+    -----
+    .. versionadded:: 0.1.0
 
     """
-    # load the sample data
+    # Load the sample data.
     sample = ww3_global_smc()
 
-    # create the mesh from the sample data
+    # Create the mesh from the sample data.
     mesh = gv.Transform.from_unstructured(sample.lons, sample.lats, data=sample.data)
-
-    # provide mesh diagnostics via logging
+    # sphinx_gallery_start_ignore
+    # Provide mesh diagnostics via logging.
     gv.logger.info("%s", mesh)
+    # sphinx_gallery_end_ignore
 
-    # threshold the mesh of NaNs
+    # Threshold the mesh of NaNs.
     mesh = mesh.threshold()
 
-    # plot the mesh
+    # Plot the unstructured mesh.
     plotter = gv.GeoPlotter()
     sargs = {"title": f"{sample.name} / {sample.units}", "shadow": True}
     plotter.add_mesh(mesh, scalar_bar_args=sargs)
