1aeb64a775e920dfb2520bd8ed39285b6bb70163
Bill Little <bill.james.little@gmail.com>
2023-12-24 00:44:34 +0000

    reformat fesom examples (#599)

--- src/geovista/examples/unstructured/fesom_fouc.py
+++ src/geovista/examples/unstructured/fesom_fouc.py
@@ -4,13 +4,29 @@
 # This file is part of GeoVista and is distributed under the 3-Clause BSD license.
 # See the LICENSE file in the package root directory for licensing details.
 
-"""Importable and runnable geovista example.
-
-Notes
------
-.. versionadded:: 0.1.0
-
 """
+FESOM Mesh (Projected)
+----------------------
+
+This example demonstrates how to render a projected unstructured mesh.
+
+📋 Summary
+^^^^^^^^^^
+
+Creates a mesh from 2-D latitude and longitude unstructured cell bounds.
+
+The resulting mesh is formed from masked connectivity, allowing the mesh to contain
+mixed cell geometries up to 18-sides (octadecagon).
+
+It uses a AWI Climate Model (AWI-CI) Finite Element Sea ice-Ocean Model (FESOM)
+v1.4 unstructured mesh of surface sea temperature data. The data targets the
+mesh faces/cells.
+
+Note that, a Natural Earth base layer is rendered along with Natural Earth
+coastlines, and the mesh is also transformed to the Foucaut pseudo-cylindrical
+projection.
+
+"""  # noqa: D205,D212,D400
 from __future__ import annotations
 
 import geovista as gv
@@ -19,24 +35,17 @@ import geovista.theme
 
 
 def main() -> None:
-    """Create a mesh from 2-D latitude and longitude unstructured cell bounds.
+    """Plot a projected FESOM unstructured mesh.
 
-    The resulting mesh is formed from masked connectivity, allowing the mesh to contain
-    mixed cell geometries up to 18-sides (octadecagon).
-
-    It uses a AWI Climate Model (AWI-CI) Finite Element Sea ice-Ocean Model (FESOM)
-    v1.4 unstructured mesh of surface sea temperature data. The data targets the
-    mesh faces/cells.
-
-    Note that, a Natural Earth base layer is rendered along with Natural Earth
-    coastlines, and the mesh is also transformed to the Foucaut pseudo-cylindrical
-    projection.
+    Notes
+    -----
+    .. versionadded:: 0.1.0
 
     """
-    # load the sample data
+    # Load the sample data.
     sample = fesom()
 
-    # create the mesh from the sample data
+    # Create the mesh from the sample data.
     mesh = gv.Transform.from_unstructured(
         sample.lons,
         sample.lats,
@@ -44,16 +53,17 @@ def main() -> None:
         data=sample.data,
         clean=True,
     )
-
-    # provide mesh diagnostics via logging
+    # sphinx_gallery_start_ignore
+    # Provide mesh diagnostics via logging.
     gv.logger.info("%s", mesh)
+    # sphinx_gallery_end_ignore
 
-    # plot the mesh
+    # Plot the unstructured mesh.
     crs = "+proj=fouc"
     plotter = gv.GeoPlotter(crs=crs)
     sargs = {"title": f"{sample.name} / {sample.units}", "shadow": True}
-    # require increased relative tolerance accuracy when cutting the mesh
-    # at the anti-meridian due to its complex geometry
+    # Require increased relative tolerance accuracy when cutting the mesh
+    # at the anti-meridian due to its complex geometry.
     plotter.add_mesh(mesh, scalar_bar_args=sargs, rtol=1e-8)
     plotter.add_base_layer(texture=gv.natural_earth_hypsometric())
     plotter.add_coastlines()
