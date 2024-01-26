884d6baed97aa96cddbdc5e0803658bbffc7c1d5
Bill Little <bill.james.little@gmail.com>
2024-01-26 00:15:53 +0000

    tidy geovista module namespace (#671)

--- src/geovista/__init__.pyi
+++ src/geovista/__init__.pyi
@@ -1,6 +1,5 @@
 # see https://scientific-python.org/specs/spec-0001/#type-checkers
 from .bridge import Transform
-from .geodesic import BBox, line, panel, wedge
 from .geoplotter import GeoPlotter
 from .pantry.textures import (
     blue_marble,
@@ -11,15 +10,11 @@ from .pantry.textures import (
 from .report import Report
 
 __all__ = [
-    "BBox",
     "GeoPlotter",
     "Report",
     "Transform",
     "blue_marble",
     "checkerboard",
-    "line",
     "natural_earth_1",
     "natural_earth_hypsometric",
-    "panel",
-    "wedge",
 ]
