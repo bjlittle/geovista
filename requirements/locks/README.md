22da0ac12023021be6680f4c193fd139d6420ead
Bill Little <bill.james.little@gmail.com>
2024-01-29 16:15:45 +0000

    support lock to yaml

--- requirements/locks/README.md
+++ requirements/locks/README.md
@@ -1,5 +1,7 @@
 # ⚠️
 
-This directory contains auto-generated `conda-lock` environment files for each `python` distribution supported by `geovista`.
+This directory contains auto-generated `conda-lock` (`*.txt`) environment files for each `python` distribution supported by `geovista`.
+
+An equivalent explicit YAML (`*.yml`) file is also generated for each `python` distribution.
 
 Please **do not** manually edit these files as they will be overwritten by the `ci-locks` GHA CI workflow.
---
+++ requirements/locks/lock2yaml.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Copyright (c) 2021, GeoVista Contributors.
+#
+# This file is part of GeoVista and is distributed under the 3-Clause BSD license.
+# See the LICENSE file in the package root directory for licensing details.
+
+"""Convert a lock file to a YAML file."""
+from __future__ import annotations
+
+from pathlib import Path
+import sys
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+if (nargv := (len(sys.argv) - 1)) != 1:
+    emsg = f"Expect 'environment-name' as a single argument to '{sys.argv[0]}' script."
+    raise ValueError(emsg)
+
+environment = Environment(
+    autoescape=select_autoescape(), loader=FileSystemLoader("templates/")
+)
+template = environment.get_template("lock2yaml.txt")
+
+# default to linux-64 only
+env = sys.argv[1]
+lock = f"{env}-linux-64.txt"
+name = f"geovista-{env}"
+yaml = f"{env}-linux-64.yml"
+
+with Path.open(lock) as fin:
+    content = template.render(file=fin, name=name)
+
+with Path.open(yaml, mode="w", encoding="utf-8") as fout:
+    fout.write(content)
