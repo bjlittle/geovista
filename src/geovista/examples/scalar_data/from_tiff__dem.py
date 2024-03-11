9acb824116e99cee70c566f6a0864ef0f33aeaa8
Bill Little <bill.james.little@gmail.com>
2024-03-11 11:12:28 +0000

    reformat from_tiff__dem example

--- src/geovista/examples/scalar_data/from_tiff__dem.py
+++ src/geovista/examples/scalar_data/from_tiff__dem.py
@@ -8,7 +8,7 @@
 GeoTIFF DEM
 -----------
 
-This example demonstrates how to render Digital Elevation Model (DEM) data
+This example demonstrates how to render **Digital Elevation Model** (DEM) data
 encoded in an `OGC GeoTIFF <https://www.ogc.org/standard/geotiff/>`_ as a
 geolocated mesh.
 
@@ -17,22 +17,22 @@ geolocated mesh.
 
 Creates an elevation warped mesh from GeoTIFF encoded DEM data.
 
-The data is sourced from the USGS Earth Resources Observation and Science
+The data is sourced from the **USGS Earth Resources Observation and Science**
 (EROS) Archive, which is freely available from the
 `USGS EarthExplorer <https://earthexplorer.usgs.gov>`_.
 
-The Digital Elevation data was collected as part of the Shuttle
-Radar Topography Mission (SRTM) aboard the space shuttle Endeavour,
+The Digital Elevation Model data was collected as part of the **Shuttle
+Radar Topography Mission** (SRTM) aboard the space shuttle *Endeavour*,
 February 11-22, 2000.
 
 Endeavour orbited Earth 16 times each day during the 11-day mission,
 completing 176 orbits. SRTM successfully collected radar data over 80%
-of the Earth's land surface between 60° north and 56° south latitude with
+of the Earth's land surface between 60°N and 56°S latitude with
 data points posted every 1 arc-second (approximately 30 meters).
 
-This SRTM sample is of Mount Fuji, one of Japan's
+This SRTM sample is of *Mount Fuji*, one of Japan's
 `Three Holy Mountains <https://blog.japanwondertravel.com/japans-three-sacred-mountains-14297>`_,
-which is an active stratovolcano, located on the Japanese island of Honshū.
+which is an active stratovolcano, located on the Japanese island of *Honshū*.
 
 The resulting mesh contains quad cells, with the elevation data located
 on the mesh nodes/points.
