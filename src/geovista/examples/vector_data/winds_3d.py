c3f25700822a5e0606322f2a75c587bf5798e88c
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2025-01-29 18:41:40 +0000

    Fix vector examples text and make runnable as gallery examples.

--- src/geovista/examples/vector_data/winds_3d.py
+++ src/geovista/examples/vector_data/winds_3d.py
@@ -38,39 +38,45 @@ from __future__ import annotations
 import geovista as gv
 from geovista.pantry.data import lfric_winds
 
-# get sample data
-sample = lfric_winds()
+
+def main() -> None:
+    """Demonstrate 3-dimensional wind arrows plotting."""
+    # get sample data
+    sample = lfric_winds()
+
+    # Create a mesh of individual points, adding vectors at each point.
+    mesh = gv.Transform.from_points(
+        sample.lons,
+        sample.lats,
+        # supply all three components, but with a big extra scaling on the "W" values
+        vectors=(sample.u, sample.v, 1500.0 * sample.w),
+        # offset from surface to avoid downward-pointing arrows disappearing
+        radius=1.1,
+    )
+
+    # Create a new mesh containing arrow glyphs, from the mesh vectors.
+    # NOTE: choose an overall scaling factor to make the arrows a reasonable size.
+    arrows = mesh.glyph(factor=0.02)
+
+    # Add the arrows to a Plotter with other aspects, and display
+    p = gv.GeoPlotter()
+    # Scale the base layer slightly to ensure it remains 'below' other elements.
+    p.add_base_layer(radius=0.99)
+    p.add_mesh(arrows, cmap="inferno")
+    p.add_coastlines()
+    p.add_graticule()
+    p.add_axes()
+
+    # Set up a suitable camera view
+    p.camera.zoom(1.3)
+    selected_view = [
+        (0.6917810912064826, -3.065688850990997, 0.4317999141924935),
+        (0.41358279170396495, 0.07362917740509836, 0.5091223320854129),
+        (0.8088496364623022, 0.05726400555597287, 0.5852205560833343),
+    ]
+    p.camera_position = selected_view
+    p.show()
 
 
-# Create a mesh of individual points, adding vectors at each point.
-mesh = gv.Transform.from_points(
-    sample.lons,
-    sample.lats,
-    # supply all three components, but with a big extra scaling on the "W" values
-    vectors=(sample.u, sample.v, 1500.0 * sample.w),
-    # offset from surface to avoid downward-pointing arrows disappearing
-    radius=1.1,
-)
-
-# Create a new mesh containing arrow glyphs, from the mesh vectors.
-# NOTE: choose an overall scaling factor to make the arrows a reasonable size.
-arrows = mesh.glyph(factor=0.02)
-
-# Add the arrows to a plotter with other aspects, and display
-plotter = gv.GeoPlotter()
-# Scale the base layer slightly to ensure it remains 'below' other elements.
-plotter.add_base_layer(radius=0.99)
-plotter.add_mesh(arrows, cmap="inferno")
-plotter.add_coastlines()
-plotter.add_graticule()
-plotter.add_axes()
-
-# Set up a suitable camera view
-plotter.camera.zoom(1.3)
-selected_view = [
-    (0.6917810912064826, -3.065688850990997, 0.4317999141924935),
-    (0.41358279170396495, 0.07362917740509836, 0.5091223320854129),
-    (0.8088496364623022, 0.05726400555597287, 0.5852205560833343),
-]
-plotter.camera_position = selected_view
-plotter.show()
+if __name__ == "__main__":
+    main()
