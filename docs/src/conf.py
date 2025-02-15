8dd8ccbffe9d0ee33cb25310375a5e9300d2f23e
Bill Little <bill.james.little@gmail.com>
2025-02-15 01:38:49 +0000

    docs: update pyvista intersphinx mapping

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -526,7 +526,7 @@ intersphinx_mapping = {
     "pooch": ("https://www.fatiando.org/pooch/latest/", None),
     "pyproj": ("https://pyproj4.github.io/pyproj/stable/", None),
     "python": ("https://docs.python.org/3/", None),
-    "pyvista": ("https://docs.pyvista.org/version/stable/", None),
+    "pyvista": ("https://docs.pyvista.org/", None),
     "pyvistaqt": ("https://qtdocs.pyvista.org/", None),
     "rasterio": ("https://rasterio.readthedocs.io/en/stable/", None),
     "requests": ("https://requests.readthedocs.io/en/stable/", None),
