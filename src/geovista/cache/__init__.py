dc673f30957e46bc4f237ad7962a8be91e123582
Bill Little <bill.james.little@gmail.com>
2025-03-25 22:28:39 +0000

    cli: add clouds stratify example (#1386)
    * cli: add clouds stratify example
    
    * added changelog news fragment

--- src/geovista/cache/__init__.py
+++ src/geovista/cache/__init__.py
@@ -44,7 +44,7 @@ FileLike: TypeAlias = str | IO[AnyStr]
 BASE_URL: str = "https://github.com/bjlittle/geovista-data/raw/{version}/assets/"
 """Base URL for :mod:`geovista` resources."""
 
-DATA_VERSION: str = "2025.03.6"
+DATA_VERSION: str = "2025.03.7"
 """The ``geovista-data`` repository version for :mod:`geovista` resources."""
 
 GEOVISTA_CACHEDIR: str = "GEOVISTA_CACHEDIR"
