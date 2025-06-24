7300f9ef5734b1a22758b11d89e4c0f842524cdc
Bill Little <bill.james.little@gmail.com>
2025-06-24 23:17:09 +0100

    tidy `geovista.core` post pyvista 0.45 (#1516)
    * tidy core post pyvista 0.45
    
    * add changelog news fragment

--- src/geovista/core.py
+++ src/geovista/core.py
@@ -19,7 +19,6 @@ from typing import TYPE_CHECKING
 import warnings
 
 import lazy_loader as lazy
-import pyvista as pv
 
 from .common import (
     CENTRAL_MERIDIAN,
@@ -379,9 +378,7 @@ def combine(
         common_point_data = set(first.point_data.keys())
         common_cell_data = set(first.cell_data.keys())
         common_field_data = set(first.field_data.keys())
-        active_scalars_info = {
-            pv.core.dataset.ActiveArrayInfoTuple(*first.active_scalars_info)
-        }
+        active_scalars_info = {first.active_scalars_info}
 
     for i, mesh in enumerate(meshes):
         if not isinstance(mesh, pv.PolyData):
@@ -430,9 +427,7 @@ def combine(
             common_cell_data &= set(mesh.cell_data.keys())
             common_field_data &= set(mesh.field_data.keys())
             if mesh.active_scalars_name:
-                active_scalars_info &= {
-                    pv.core.dataset.ActiveArrayInfoTuple(*mesh.active_scalars_info)
-                }
+                active_scalars_info &= {mesh.active_scalars_info}
 
     points = np.vstack(combined_points)
     faces = np.hstack(combined_faces)
