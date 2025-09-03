ce980d56b9279dcf345d2f726f948ac3ef85ce66
Bill Little <bill.james.little@gmail.com>
2025-09-03 12:24:37 +0100

    adopt ruff cpy preview rule

--- src/geovista/__init__.py
+++ src/geovista/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2021, GeoVista Contributors.
+# Copyright (c) 2021, GeoVista Contributors.
 #
 # This file is part of GeoVista and is distributed under the 3-Clause BSD license.
 # See the LICENSE file in the package root directory for licensing details.
