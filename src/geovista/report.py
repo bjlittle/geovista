0fa6c79f8d1dfa3472abc6c0c28ff9bddae9875c
Bill Little <bill.james.little@gmail.com>
2025-10-17 16:25:49 +0100

    refresh report packages (#1763)
    * refresh report packages
    
    * add changelog news fragment

--- src/geovista/report.py
+++ src/geovista/report.py
@@ -45,9 +45,11 @@ PACKAGES_CORE: list[str] = [
     "cmocean",
     "colorcet",
     "geovista",
+    "lazy-loader",
     "matplotlib",
     "netcdf4",
     "numpy",
+    "pillow",
     "platformdirs",
     "pooch",
     "pykdtree",
@@ -59,28 +61,29 @@ PACKAGES_CORE: list[str] = [
 """The core packages of geovista to include in the environment report."""
 
 PACKAGES_OPTIONAL: list[str] = [
-    "IPython",
-    "PyQt5",
+    # optional
     "fastparquet",
     "h3",
-    "imageio",
-    "ipywidgets",
-    "jupyter_server_proxy",
-    "jupyterlab",
-    "meshio",
-    "nest_asyncio",
     "pandas",
+    "pyqt",
     "pyvistaqt",
     "rasterio",
+    # miscellaneous
+    "ipython",
+    "imageio",
+    "meshio",
     "scipy",
-    "trame",
-    "trame_client",
-    "trame_common",
-    "trame_jupyter_extension",
-    "trame_server",
-    "trame_vtk",
-    "trame_vuetify",
+    # jupyter related
+    "ipywidgets",
+    "jupyter-server-proxy",
+    "jupyterlab",
+    "nest-asyncio",
     "tqdm",
+    "trame",
+    "trame-client",
+    "trame-server",
+    "trame-vtk",
+    "trame-vuetify",
     "wslink",
 ]
 """The optional packages of geovista to include in the environment report."""
@@ -104,6 +107,7 @@ class Report(scooby.Report):  # numpydoc ignore=PR01
         additional: PackageLike | list[PackageLike] | None = None,
         ncol: int | None = None,
         text_width: int | None = None,
+        sort: bool | None = True,
         gpu: bool | None = True,
     ) -> None:
         """Generate an environment package and hardware report.
@@ -118,6 +122,8 @@ class Report(scooby.Report):  # numpydoc ignore=PR01
         text_width : int, optional
             The number of character columns in a non-HTML report. Defaults to
             :data:`TEXT_WIDTH`.
+        sort : bool, optional
+            Alphabetically sort the packages. Defaults to ``True``.
         gpu : bool, optional
             Detect GPU hardware details. Defaults to ``True``. Disable this option if
             experiencing rendering issues to ensure report generation.
@@ -159,5 +165,6 @@ class Report(scooby.Report):  # numpydoc ignore=PR01
             optional=optional,
             ncol=ncol,
             text_width=text_width,
+            sort=sort,
             extra_meta=extra_meta,
         )
