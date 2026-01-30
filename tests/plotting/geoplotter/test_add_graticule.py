2330fb1b6837ae6bbe39e906fa9b220a12fcc12f
ukmo-ccbunney <christopher.bunney@metoffice.gov.uk>
2026-01-30 13:18:49 +0000

    Reduced number of image tests.

--- tests/plotting/geoplotter/test_add_graticule.py
+++ tests/plotting/geoplotter/test_add_graticule.py
@@ -11,49 +11,34 @@ import pytest
 
 import geovista as gv
 
-CRS = [None, "moll", "poly", "ups"]
-
 
 @pytest.mark.image
-@pytest.mark.parametrize("crs", CRS)
 @pytest.mark.parametrize(
-    "n_samples",
-    [
-        pytest.param(None, id="default"),
-        pytest.param((None, 5), id="coarse_lat"),
-        pytest.param((5, None), id="coarse_lon"),
-        pytest.param((15, 10), id="lowres"),
-        pytest.param((720, 360), id="highres"),
-    ],
+    "n_samples", [(12, 6), (None, 6)], ids=["coarse", "coarse_lat"]
 )
-def test_graticule_samples(plot_nodeid, verify_image_cache, crs, n_samples):
+def test_graticule_samples(plot_nodeid, verify_image_cache, n_samples):
     """Test number of samples used for graticule lines."""
     verify_image_cache.test_name = plot_nodeid
 
-    tgt_crs = f"+proj={crs}" if crs else None
+    tgt_crs = "+proj=poly"
     p = gv.GeoPlotter(crs=tgt_crs)
     p.add_base_layer()
     p.add_graticule(n_samples=n_samples)
-    if crs:
-        # projected plots look better in the XY plane:
-        p.camera_position = "xy"
+    p.camera_position = "xy"
     p.camera.zoom(1.5)
     p.show()
 
 
 @pytest.mark.image
-@pytest.mark.parametrize("crs", CRS)
-@pytest.mark.parametrize("factor", [None, 0.1, 2, 100])
-def test_graticule_factor(plot_nodeid, verify_image_cache, crs, factor):
+@pytest.mark.parametrize("factor", [0.1, 10])
+def test_graticule_factor(plot_nodeid, verify_image_cache, factor):
     """Test graticule sample scaling factor."""
     verify_image_cache.test_name = plot_nodeid
 
-    tgt_crs = f"+proj={crs}" if crs else None
+    tgt_crs = "+proj=poly"
     p = gv.GeoPlotter(crs=tgt_crs)
     p.add_base_layer()
     p.add_graticule(factor=factor)
-    if crs:
-        # projected plots look better in the XY plane:
-        p.camera_position = "xy"
+    p.camera_position = "xy"
     p.camera.zoom(1.5)
     p.show()
