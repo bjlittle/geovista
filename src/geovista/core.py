83e68f7b4c5aaac82d9321bec80efdab6c25290e
Bill Little <bill.james.little@gmail.com>
2026-05-06 16:29:29 +0100

    add mesh honours active scalar name

--- src/geovista/core.py
+++ src/geovista/core.py
@@ -34,7 +34,6 @@ from .common import (
     distance,
     from_cartesian,
     point_cloud,
-    sanitize_vtk,
     to_cartesian,
     wrap,
 )
@@ -261,7 +260,7 @@ class MeridianSlice:  # numpydoc ignore=PR01
                 lonlat = from_cartesian(mesh)
                 match = np.abs(lonlat[:, 0] - self.meridian) < 90
                 mesh = cast(mesh.extract_points(match))
-            sanitize_vtk(mesh)
+
             if mesh.n_cells:
                 mesh.set_active_scalars(
                     self._info.name, preference=self._info.association.name.lower()
